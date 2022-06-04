7b0402b795815672e0eb01f2ec675fa76e4dd1a7
parthibeyond <parthi@beyondfinity.com>
2022-06-05 02:08:52 +0530

    Allow pull_request & commit to build changes history

--- lib/github/commit.rb
+++ lib/github/commit.rb
@@ -1,2 +1,29 @@
 class Commit < Base
+  attr_accessor :files, :diffs
+  
+  def initialize(options = {})
+    super
+    fetch_from_html_source('commit', id)
+  end
+
+  def find_changed_lines
+    files = parsed_html.search('#files .file')
+    parsed_html.search('#files include-fragment').map { |frag| files += retrieve_fragments(frag.attr('src')).search('.file') }
+    @diffs = files.map do |file|
+      title = file.at_css('.Link--primary').text
+      numbers = file.search('.blob-num-addition.js-linkable-line-number').map{|line| line.attributes['data-line-number'].value }
+      [title, numbers]
+    end
+  end
+
+  def retrieve_fragments(fragment_url)
+    url = "https://github.com/#{fragment_url}"
+    response = RestClient.get url
+    formetted_response = "<html><body>#{response.body}</html></body>"
+    Nokogiri.parse formetted_response
+  end
+
+  def parsed_html
+    Nokogiri.parse retrieved_html
+  end
 end
--- lib/github/pull_request.rb
+++ lib/github/pull_request.rb
@@ -1,2 +1,30 @@
 class PullRequest < Base
+  attr_reader :commits, :changes_history
+
+  def initialize(options = {})
+    super
+    fetch_from_api_source('pulls', id)
+    build_commits
+  end
+
+  def build_commits
+    @commits = retrieve_all_commits.map { |commit| Commit.new(id: commit['sha']) }
+  end
+  
+  def changes_history
+    @changes_history ||= generate_changes_history
+  end
+  
+  private
+
+  def retrieve_all_commits
+    response = RestClient.get retrieved_object['commits_url']
+    JSON.parse response
+  end
+  
+  def generate_changes_history
+    @commits.each do |commit|
+      changes_history << commit.find_changed_lines
+    end
+  end
 end
